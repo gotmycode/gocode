500830ebdff781f667187d8a0d5254b53a729c33
Yvan Go <Galaxy@Yvans-Air.fritz.box>
2018-05-22 18:01:11 +0200

    added deck.go

---
+++ cards/deck.go
@@ -0,0 +1,6 @@
+package main
+
+//create a new type of 'deck'
+//which is a slice of strings
+//in OO world, this new deck type borrows / extends behaviour of slice of type string
+type deck []string
